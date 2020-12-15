ccd37800e37ed229a04585345d3a7fd9e03ebd44
Govorkoff <govorkoff@citydom.ru>
2020-12-15 07:55:30 +0300

    11.start

--- gov/src/App.js
+++ gov/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      
+      <header>
+        <img src="https://a.deviantart.net/avatars/j/a/jack070.png?7"></img>
       </header>
+      
+      <nav>
+        <div>Profile</div>
+        <div>Message</div>
+      </nav>
+
+      <div>
+      </div>
+
+
     </div>
   );
 }
